dbf0faa02156bb0b9a09583c419d4dc1e1fbbedf
Peter Dowdy <peter.dowdy@gmail.com>
2020-12-16 22:34:10 -0800

    Day 17

---
+++ Day17/Part1.py
@@ -0,0 +1,49 @@
+text = open('Day17/input.txt').readlines()
+grid = {}
+for y in range(0,len(text[0].replace('\n',''))):
+    for x in range(0,len(text[y].replace('\n',''))):
+        grid[(x,y,0)] = text[y][x]
+
+for x in range(0,6):
+    newgrid = {}
+    candidates = list(grid.items())
+    for k,v in candidates:
+        for x in range(k[0]-1,k[0]+2):
+            for y in range(k[1]-1,k[1]+2):
+                for z in range(k[2]-1,k[2]+2):
+                    if (x,y,z) not in grid:
+                        grid[(x,y,z)] = '.'
+                        continue
+    candidates = list(grid.items())
+    for k,v in candidates:
+        active = 0
+        shouldBreak = False
+        for x in range(k[0]-1,k[0]+2):
+            if shouldBreak:
+                break
+            for y in range(k[1]-1,k[1]+2):
+                if shouldBreak:
+                    break
+                for z in range(k[2]-1,k[2]+2):
+                    if shouldBreak:
+                        break
+                    if k == (x,y,z):
+                        continue
+                    if (x,y,z) not in grid:
+                        continue
+                    if grid[(x,y,z)] == '#':
+                        active += 1
+                    if active > 3:
+                        shouldBreak = True
+        if active in [2,3] and grid[k] == '#':
+            newgrid[k] = '#'
+        elif active == 3:
+            newgrid[k] = '#'
+        else:
+            newgrid[k] = '.'
+    grid = newgrid
+active = 0
+for x in grid.values():
+    if x == '#':
+        active += 1
+print(active)
